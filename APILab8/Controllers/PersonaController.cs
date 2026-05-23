b64ce4eb76b2ef6a737f929cbe8ff4695d6dff5d
jusMa13 <jusherramarenco@gmail.com>
2026-05-22 18:38:25 -0600

    Creacion de el apartad de personaControllers

---
+++ APILab8/Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using Contactos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILab8.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonaController : Controller
+    {
+        private readonly string _connectionString;
+
+        public PersonaController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CadenaContactos");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var personas = await Contactos.Models.Persona.ObtenerTodas(_connectionString);
+            return Ok(personas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Persona nuevaPersona)
+        {
+            var exito = await Persona.Insertar(_connectionString, nuevaPersona);
+            if (exito)
+            {
+                return Ok(new {mensaje = "Persona insertada correctamente" });
+            }
+            return BadRequest(new {mensaje = "Error al insertar persona" });
+        }
+
+    }
+}
--- APILab8/Middleware.cs
+++ APILab8/Middleware.cs
@@ -4,16 +4,39 @@ using System.Threading.Tasks;
 
 namespace APILab8
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    
     public class Middleware
     {
         private readonly RequestDelegate _next;
+        private const string APIKEYNAME = "X-API-KEY";
+
 
         public Middleware(RequestDelegate next)
         {
             _next = next;
         }
 
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEYNAME,out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Falta la clave de API");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>("ApiKeySettings:ApiKey");
+
+           if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Clave de API no válida");
+                return;
+
+            }
+            await _next(context);
+        }
         public Task Invoke(HttpContext httpContext)
         {
 
